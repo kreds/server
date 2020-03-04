dbbe9f30e1186e6c823ff73b95355ebf04ce67db
Mat Sz <contact@matsz.dev>
2020-03-04 20:47:25 +0100

    added database connection

--- src/App.ts
+++ src/App.ts
@@ -1,5 +1,7 @@
 import 'reflect-metadata';
 import { createKoaServer } from 'routing-controllers';
+import { createConnection, useContainer } from 'typeorm';
+import { Container } from 'typedi';
 
 import { AuthenticationController } from './controllers/AuthenticationController';
 import { ApplicationController } from './controllers/ApplicationController';
@@ -7,20 +9,34 @@ import { GroupController } from './controllers/GroupController';
 import { OAuth2Controller } from './controllers/OAuth2Controller';
 import { UserController } from './controllers/UserController';
 import { ErrorHandler } from './middlewares/ErrorHandler';
+import ormconfig from '../ormconfig';
 
-export default function App() {
-    const app = createKoaServer({
-        cors: true,
-        controllers: [
-            AuthenticationController,
-            ApplicationController,
-            GroupController,
-            OAuth2Controller,
-            UserController,
-        ],
-        middlewares: [ ErrorHandler ],
-        defaultErrorHandler: false,
-    });
-    
-    app.listen(process.env.HTTP_PORT || 8080, process.env.HTTP_IP || '127.0.0.1');
+export default async function App() {
+    useContainer(Container);
+    try {
+        await createConnection({
+            ...ormconfig as any,
+        });
+
+        const app = createKoaServer({
+            cors: true,
+            controllers: [
+                AuthenticationController,
+                ApplicationController,
+                GroupController,
+                OAuth2Controller,
+                UserController,
+            ],
+            middlewares: [ ErrorHandler ],
+            defaultErrorHandler: false,
+        });
+        
+        const port = process.env.HTTP_PORT || 8080;
+        const ip = process.env.HTTP_IP || '127.0.0.1';
+        app.listen(port, ip);
+        
+        console.log('kreds/server listening on: ' + ip + ':' + port);
+    } catch (e) {
+        console.error(e);
+    }
 }
