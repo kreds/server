a3f39321b347e41ec7d0ba0d23ee8b1b1bec0fac
Mat Sz <contact@matsz.dev>
2020-05-22 18:49:31 +0200

    added CRUD skeleton for users

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,4 +1,31 @@
-import { Controller } from 'routing-controllers';
+import { Controller, Get, Post, Put, Delete } from 'routing-controllers';
+import { Inject } from 'typedi';
+
+import { UserService } from '../services/UserService';
 
 @Controller('/v1/users')
-export class UserController {}
+export class UserController {
+  @Inject()
+  private userService: UserService;
+
+  @Get('/')
+  async index() {
+    // TODO: Check if authenticated and allowed to do that.
+    return await this.userService.getAllUsers();
+  }
+
+  @Post('/')
+  async create() {
+    // TODO: Check if authenticated and allowed to do that.
+  }
+
+  @Put('/:id')
+  async update() {
+    // TODO: Check if authenticated and allowed to do that.
+  }
+
+  @Delete('/:id')
+  async delete() {
+    // TODO: Check if authenticated and allowed to do that.
+  }
+}
