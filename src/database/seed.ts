8d151f9e2b77f6f4f6db15d4e9fc97f430cd66b5
Mat Sz <contact@matsz.dev>
2020-05-25 18:17:27 +0200

    added new permissions to the seeder

--- src/database/seed.ts
+++ src/database/seed.ts
@@ -21,9 +21,18 @@ export async function seed(ormconfig: any) {
   superuserGroup.permissions = ['*'];
   await groupRepository.save(superuserGroup);
 
+  const developersGroup = new Group();
+  developersGroup.name = 'Developers';
+  developersGroup.permissions = [
+    'applications.create',
+    'applications.own.*',
+    'authentication.authenticate',
+  ];
+  await groupRepository.save(developersGroup);
+
   const userGroup = new Group();
   userGroup.name = 'Users';
-  userGroup.permissions = [];
+  userGroup.permissions = ['authentication.authenticate'];
   await groupRepository.save(userGroup);
 
   const authenticationMethod = new UserAuthenticationMethod();
@@ -31,12 +40,55 @@ export async function seed(ormconfig: any) {
   authenticationMethod.subtype = AuthenticationRequestPasswordSubtype.BCRYPT;
   authenticationMethod.data = hashSync('admin', 12);
 
-  const systemPermissions = ['users.list'];
+  const systemPermissions: { name: string; description: string }[] = [
+    { name: 'authentication.authenticate', description: 'Log in.' },
+    { name: 'users.list', description: 'See a list of users.' },
+    { name: 'users.create', description: 'Create a new user.' },
+    { name: 'users.update', description: 'Update an existing user.' },
+    { name: 'users.delete', description: 'Delete an existing user.' },
+    { name: 'groups.list', description: 'See a list of groups.' },
+    { name: 'groups.create', description: 'Create a new group.' },
+    { name: 'groups.update', description: 'Update an existing group.' },
+    { name: 'groups.delete', description: 'Delete an existing group.' },
+    { name: 'permissions.list', description: 'See a list of permissions.' },
+    { name: 'permissions.create', description: 'Create a new permission.' },
+    {
+      name: 'permissions.update',
+      description: 'Update an existing permission.',
+    },
+    {
+      name: 'permissions.delete',
+      description: 'Delete an existing permission.',
+    },
+    { name: 'applications.list', description: 'See a list of applications.' },
+    { name: 'applications.create', description: 'Create a new application.' },
+    {
+      name: 'applications.update',
+      description: 'Update any existing application.',
+    },
+    {
+      name: 'applications.delete',
+      description: 'Delete any existing application.',
+    },
+    {
+      name: 'applications.own.list',
+      description: 'See owned applications.',
+    },
+    {
+      name: 'applications.own.update',
+      description: 'Update an owned application.',
+    },
+    {
+      name: 'applications.own.delete',
+      description: 'Delete an owned application.',
+    },
+  ];
 
   const permissionRepository = connection.getRepository(Permission);
   for (let perm of systemPermissions) {
     const permission = new Permission();
-    permission.name = perm;
+    permission.name = perm.name;
+    permission.description = perm.description;
     await permissionRepository.save(permission);
   }
 
